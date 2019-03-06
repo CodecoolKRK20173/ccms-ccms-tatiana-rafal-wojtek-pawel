86c1d2dc2abdb035d98b78bc8368a11f9dd72220
Wojciech Charuza <wojtek.charuza@gmail.com>
2019-03-06 10:46:50 +0100

    implement connector class

--- src/main/java/com/codecool/Dao/CrudAssignmentsDaoImpl.java
+++ src/main/java/com/codecool/Dao/CrudAssignmentsDaoImpl.java
@@ -1,4 +1,29 @@
 package com.codecool.Dao;
 
-public class CrudAssignmentsDaoImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class CrudAssignmentsDaoImpl implements  CrudAssignmentsDao {
+
+    private Connection connection;
+
+    public CrudAssignmentsDaoImpl() {
+
+    }
+
+    @Override
+    public void addAssignment(String title) {
+        connection.
+    }
+
+    private Connection initializeConnection() {
+        final String DATABASEPATH = "src/main/resources/CcMS";
+        Connector connector = new Connector(DATABASEPATH);
+        return connector.getDatabaseConnection();
+    }
+
+
 }
