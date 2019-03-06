0353cec139837734c0548163f3a276c693d811e6
Tatiana Slonimskaia <slonimskaia@gmail.com>
2019-03-06 10:20:14 +0100

    Implement class User

--- src/main/java/com/codecool/Model/User.java
+++ src/main/java/com/codecool/Model/User.java
@@ -1,4 +1,31 @@
 package com.codecool.Model;
 
-public class User {
+public abstract class User {
+    int id;
+    String name;
+    String surname;
+    String email;
+
+    public User(int id, String name, String surname, String email) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
