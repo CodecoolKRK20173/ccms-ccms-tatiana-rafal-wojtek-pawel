c2f5b05547799eef731dbd8f9425968e37682ea3
Tatiana Slonimskaia <slonimskaia@gmail.com>
2019-03-06 10:07:58 +0100

    Add empty methods

--- src/main/java/com/codecool/Dao/MentorDaoImpl.java
+++ src/main/java/com/codecool/Dao/MentorDaoImpl.java
@@ -1,4 +1,25 @@
 package com.codecool.Dao;
 
-public class MentorDaoImpl {
+import com.codecool.Model.User;
+
+import java.util.List;
+
+public class MentorDaoImpl implements UserDao{
+
+    public List<User> mentors;
+
+    public List<User> getAll() {
+        return mentors;
+    }
+
+    public void delete(int id) {
+    }
+
+    public void add(String name, String surname, String mail, String loginID) {
+    }
+
+    public void edit(int id) {
+
+    }
+
 }
--- src/main/java/com/codecool/Dao/UserDao.java
+++ src/main/java/com/codecool/Dao/UserDao.java
@@ -5,8 +5,8 @@ import com.codecool.Model.User;
 import java.util.List;
 
 public interface UserDao {
-    List<User> get();
-    void delete();
+    List<User> getAll();
+    void delete(int ids);
     void add(String name, String surname, String mail, String loginID);
     void edit(int id);
 }
